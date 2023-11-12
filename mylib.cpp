4dbc274bab021f190ab9906abb1ba79b0325908e
docinlt <150606690+docinlt@users.noreply.github.com>
2023-11-12 18:08:40 +0200

    Add files via upload

---
+++ mylib.cpp
@@ -0,0 +1,97 @@
+﻿#include "mylib.h"
+
+studentas::studentas() {
+	cout << "Iveskite studento varda: "; cin >> vard;
+	cout << "Iveskite studento pavarde: "; cin >> pav;
+	cout << "Kiek pazymiu buvo semestre? ";
+	int n; cin >> n;
+	for (int i = 0; i < n; i++) {
+		int k;
+		cout << "Ivesk " << i + 1 << " semestro pazymi: "; cin >> k; paz.push_back(k);
+	}
+	cout << "Iveskite egzamino pazymi: "; cin >> egz;
+	skaiciavimo_Strategija == 'm' ? rezMed() : rezVid(); 
+}
+
+studentas::studentas(string v, string p, vector<int> pp, int e) 
+{
+	vard = v; pav = p; paz = pp; egz = e;
+	skaiciavimo_Strategija == 'm' ? rezMed() : rezVid();
+}
+studentas::studentas(const studentas& temp) 
+{
+	vard = temp.vard; pav = temp.pav;
+	paz = temp.paz; egz = temp.egz;
+	skaiciavimo_Strategija == 'm' ? rezMed() : rezVid();
+}
+studentas& studentas:: operator=(const studentas& temp) 
+{
+	if (this == &temp) return *this;
+	vard = temp.vard; pav = temp.pav;
+	paz = temp.paz; egz = temp.egz;
+	skaiciavimo_Strategija == 'm' ? rezMed() : rezVid();
+	return *this;
+}
+studentas::~studentas() 
+{
+	vard.clear(); pav.clear(); paz.clear();
+	egz = 0; rez = 0;
+}
+
+void studentas::printas() {
+	printf("|%-10s|%-20s|", vard.c_str(), pav.c_str());
+	for (auto& a : paz) printf("%3d|", a);
+	printf("%10d|\n", egz);
+}
+
+void studentas::printasRez() { 
+	printf("|%-10s|%-20s|", vard.c_str(), pav.c_str());
+	printf("%10.2f|\n", rez); 
+}
+
+void studentas::rezVid() {
+	// rez = vid * 0.4 + egz * 0.6;
+	float sum = std::accumulate(paz.begin(), paz.end(), 0.0);
+	rez = sum / paz.size() * 0.4 + egz * 0.6;
+}
+
+void studentas::rezMed() {
+	// rez = vid * 0.4 + egz * 0.6;
+	rez = mediana(paz) * 0.4 + egz * 0.6;
+}
+
+double studentas::mediana(vector<int> vec) {
+	typedef vector<double>::size_type vecSize;
+	vecSize size = vec.size();
+	if (size == 0)
+		throw std::domain_error("negalima skaiciuoti medianos tusciam vektoriui");
+	sort(vec.begin(), vec.end());
+	vecSize vid = size / 2;
+	return size % 2 == 0 ? (vec[vid] + vec[vid - 1]) / 2 : vec[vid] / 1.0;
+}
+
+
+void studentas::operator>>(std::istream& input) { 
+	cout << "Iveskite studento varda: "; input >> vard;
+	cout << "Iveskite studento pavarde: "; input >> pav;
+	cout << "Kiek pazymiu buvo semestre? ";
+	int n; input >> n;
+	for (int i = 0; i < n; i++) {
+		int k;
+		cout << "Ivesk " << i + 1 << " semestro pazymi: "; input >> k; paz.push_back(k);
+	}
+	cout << "Iveskite egzamino pazymi: "; input >> egz;
+	skaiciavimo_Strategija == 'm' ? rezMed() : rezVid(); 
+}
+
+
+void studentas::operator<<(std::ostream& output) {
+	output << "Vardas: " << vard << endl;
+	output << "Pavarde: " << pav << endl;
+	output << "Pazymiai: ";
+	for (auto& a : paz) output << a << " ";
+	output << endl;
+	output << "Egzamino pazymys: " << egz << endl;
+	output << "Galutinis balas: " << fixed << setprecision(2) << rez << endl;
+}
+
---
+++ mylib.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include <iostream>
+#include <numeric>
+#include <string>
+#include <array>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::array;
+using std::vector;
+using std::setw;
+using std::left;
+using std::right;
+using std::fixed;
+using std::setprecision;
+using std::sort;
+using std::domain_error;
+
+extern char skaiciavimo_Strategija;
+extern string failoVardas;
+
+class studentas {
+	string vard, pav; 
+	vector<int> paz;  
+	int egz;		  
+	float rez;		 
+public:
+	studentas();                                        
+	studentas(string v, string p, vector<int>pp, int e);
+	studentas(const studentas& temp);					
+	studentas& operator=(const studentas& temp);		
+	~studentas();										
+
+	inline string getVardas() { return vard; }		
+	inline int getPazNr() { return paz.size(); }	   
+
+	inline void setVardas(string t) { vard = t; }      
+
+	void printas();
+	void printasRez();
+	void rezVid();
+	void rezMed(); 
+
+	double mediana(vector<int> vec);   
+
+	void operator>>(std::istream& input);  
+	void operator<<(std::ostream& out); 
+};
